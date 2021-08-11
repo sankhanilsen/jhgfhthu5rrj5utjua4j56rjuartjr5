dd01194a2ff86a51b1e664b61ac1da4abae2c463
sankhanilsen <76916705+sankhanilsen@users.noreply.github.com>
2021-08-11 21:32:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function page_one() {
     window.location = "page_one.html";
 }
+function page_3() {
+    window.location = "pagecar.html";
+}
+function page_one() {
+    window.location = "page_one.html";
+}
+function page_one() {
+    window.location = "page_one.html";
+}
 
